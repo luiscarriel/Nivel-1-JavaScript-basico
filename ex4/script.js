faba877a7faa82371c5ebc138400ccb1bfc41c95
luiscarriel <vitorluiiscarriel@gmail.com>
2022-01-03 10:30:38 -0500

    update

---
+++ ex4/script.js
@@ -0,0 +1,9 @@
+
+        function inicio() {
+            let nome = window.prompt('Qual é o seu nome?')
+            let res = window.document.getElementById('resultado')
+            // Também funciona com o comando abaixo
+            // let res = window.document.querySelector('section#resultado')
+            res.innerHTML = `<p>Olá, <strong>${nome}</strong>! É um grande prazer te conhecer! &#x1F596;`
+        }
+    
